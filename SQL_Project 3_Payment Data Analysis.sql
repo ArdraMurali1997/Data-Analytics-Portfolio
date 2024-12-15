8ac77765bbc9530e9e09bffb1976bb9c4b7c189c
Ardra M <ardramurali1@gmail.com>
2024-12-15 17:55:10 +0530

    Add files via upload
    SQL Project on Payment Data
    * This sql project is an analysis on UPI Payments
    * 5 random tables are created with the help of chatgpt
    * Table 01-Banks:bank_id, bank_name
    * Table 02-Mechants: merchant_id, Name, Address, Phone,Email,Business Type, BankID
    * Table 03-users:user_id,name, phone
    * Table 04-Disputes:dsipute_id,transaction_id,user_id,merchant_id,dispute_amount,dispute_reason, dispute_status,dispute_date, resolution_date
    * Table 05-Transactions-transaction_id,user_id,merchant_id,bank_id,transaction_amount,payment_method,transaction_status, transaction_date, remarks
    *9 situations are looked into to showcase DQL Commands, Joins, Aggregate functions, date functions and Analytics skills.

---
+++ SQL_Project 3_Payment Data Analysis.sql
@@ -0,0 +1,73 @@
+#Payment industry KPI Analysis
+#------------------------------------------------------------------------------------------
+#1. Transactions success % for each merchant
+# Transaction success rate= (Number of succesfull transactions/total number of transations)*100
+select t.merchant_id, m.name,
+concat(round(((sum(case when transaction_status="Success" then 1 else 0 end))/count(*))*100,1),"%") as success_rate
+from transactions t join merchants m
+on t.merchant_id=m.merchant_id
+group by 1,2;
+#------------------------------------------------------------------------------------------
+#2. Average Dispute Resolution time
+#(resolution date-dispute date)/no.of disputes
+select avg(datediff(dispute_date,resolution_date)) as avg_res_time from disputes;
+#------------------------------------------------------------------------------------------
+#3. Successfull Revenue by merchant with revenue rate
+select t.merchant_id, m.name,
+sum(transaction_amount) as Revenue
+from transactions t join merchants m
+on t.merchant_id=m.merchant_id
+where t.transaction_status="Success"
+group by 1,2
+order by sum(transaction_amount) desc;
+#------------------------------------------------------------------------------------------
+#4. Dispute rate per merchant
+#(Number of dispute/Number of trasactions)*100
+select m.merchant_id,m.name, 
+concat(round((count(d.dispute_id)/count(t.transaction_id))*100,1),"%") as dispute_rate
+from merchants m join transactions t
+on m.merchant_id=t.merchant_id
+left join disputes d 
+on t.transaction_id=d.transaction_id
+group by 1,2
+order by 3 desc;
+#------------------------------------------------------------------------------------------
+#5 Week-wise Revenue
+select year(transaction_date),monthname(transaction_date),week(transaction_date),
+sum(transaction_amount) from transactions
+group by 1,2,3
+order by 3;
+#------------------------------------------------------------------------------------------
+#6 Failed transaction rate by bank
+select b.bank_id,b.bank_name,
+((sum(case when t.transaction_status="Failed" then 1 else 0 end))/count(t.transaction_id))*100 as failure_rate
+from transactions t right join banks b
+on t.bank_id=b.bank_id
+group by 1,2
+order by 3 desc;
+#------------------------------------------------------------------------------------------
+#7 Top 5 merchants by revenue
+select m.merchant_id, m.name, sum(t.transaction_amount) as revenue
+from merchants m join transactions t 
+on m.merchant_id=t.merchant_id
+group by 1,2
+order by 3 desc
+limit 5;
+#------------------------------------------------------------------------------------------
+#8  Average transaction amount per user
+select u.user_id,u.name,round(avg(t.transaction_amount),2) as average_transaction
+from users u join transactions t 
+on u.user_id=t.user_id
+group by 1,2;
+#------------------------------------------------------------------------------------------
+#9 Rank dispute reason based on count of dispute
+select dispute_reason, count(*) as dispute_count,
+sum(dispute_amount) as dispute_amount from disputes
+group by 1
+order by 2 desc
+limit 3;
+#------------------------------------------------------------------------------------------
+
+
+
+
